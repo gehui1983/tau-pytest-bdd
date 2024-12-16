1371ada34eba1952482f838aa98fc5809874cd59
gehui1983 <gehui1983@126.com>
2024-12-16 21:29:48 +0800

    selenium web

--- tests/step_defs/Test_Op.py
+++ tests/step_defs/Test_Op.py
@@ -1,15 +1,6 @@
 import time
-
-from selenium import webdriver
-from selenium.webdriver import Proxy
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-# from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.proxy import ProxyType
-# from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
-selenium_grid_url = "http://127.0.0.1:4444/wd/hub"
 
 
 def add_cookie(drive):
@@ -23,30 +14,18 @@ def add_cookie(drive):
     drive.refresh()
 
 
-myproxy = "192.168.3.241:8888"
-proxy = Proxy({
-    'proxyType': ProxyType.MANUAL,
-    'httpProxy': myproxy,
-    'ftpProxy': myproxy,
-    "sslProxy": myproxy,
-    'noProxy': 'localhost,127.0.0.1'
-})
-options = ChromeOptions()
-options.set_capability("browserVersion", "131.0")
-options.set_capability("browserName", "chrome")
-options.set_capability("proxy", proxy.to_capabilities())
-# options.set_capability("sslCertificate", "/home/james/charles-ssl-proxying-certificate.pem")
 ss = str(int(time.time()))
-with webdriver.Remote(command_executor=selenium_grid_url, options=options) as driver:
-    driver.maximize_window()
-    driver.get(url='https://op.xhdigit.com/')
-    add_cookie(driver)
-    driver.get(url="https://op.xhdigit.com/#/product")
-    driver.save_screenshot("xhdigit-" + ss + "-1-" + ".png")
-    driver.find_element(By.XPATH, "/html/body/div/div[2]/div/div[1]/div[2]/button").click()
 
-    element = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.XPATH,
-                                                                       '/html/body/div/div/div[1]/ul/li[1]/div'))
 
+def test_op(chrome_browser):
+    chrome_browser.maximize_window()
+    chrome_browser.get(url='https://op.xhdigit.com/')
+    add_cookie(chrome_browser)
+    chrome_browser.get(url="https://op.xhdigit.com/#/product")
+    chrome_browser.save_screenshot("xhdigit-" + ss + "-1-" + ".png")
+    chrome_browser.find_element(By.XPATH, "/html/body/div/div[2]/div/div[1]/div[2]/button").click()
+
+    element = WebDriverWait(chrome_browser, 10).until(lambda x: x.find_element(By.XPATH,
+                                                                               '/html/body/div/div/div[1]/ul/li[1]/div'))
     print(element)
-    driver.save_screenshot("xhdigit-" + ss + "-2-" + ".png")
+    chrome_browser.save_screenshot("xhdigit-" + ss + "-2-" + ".png")
